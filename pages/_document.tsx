f9039ba66909ddde4be989f32de8e7ce0accc4e1
Samuelsonvs <m.sametatali@gmail.com>
2021-08-14 09:08:00 +0300

    performance

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,14 +7,14 @@ class MyDocument extends Document {
         <Head>
           <link
             rel="preload"
-            href="/font/montserratRegular.woff2"
+            href="/fonts/montserratRegular.woff2"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
           />
           <link
             rel="preload"
-            href="/font/playfairDisplay.woff2"
+            href="/fonts/playfairDisplay.woff2"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
