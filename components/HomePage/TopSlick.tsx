a822f10251053cb0eed817d807d38bedd5e415ce
Samuelsonvs <m.sametatali@gmail.com>
2021-08-16 15:23:11 +0300

    performance

--- components/HomePage/TopSlick.tsx
+++ components/HomePage/TopSlick.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Slider from "react-slick";
 import Image from "next/image";
 import TopSlickTexts from "./TopSlickTexts";
+import Carousel from 'react-material-ui-carousel'
 
 import Image1 from "@/public/images/topslide/1.jpg";
 import Image2 from "@/public/images/topslide/2.jpg";
@@ -10,20 +11,31 @@ import Image3 from "@/public/images/topslide/3.jpg";
 export default function TopSlick() {
   const [activeSlide, setActiveSlide] = useState(0);
   const [afterActiveSlide, setAfterActiveSlide] = useState(0);
-  const settings = {
-    infinite: true,
-    autoplay: true,
-    fade: true,
-    speed: 3000,
-    arrows: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    beforeChange: (current: any, next: any) => setActiveSlide(next),
-    afterChange: (current: any) => setAfterActiveSlide(current),
-  };
+  // const settings = {
+  //   infinite: true,
+  //   autoplay: true,
+  //   fade: true,
+  //   speed: 3000,
+  //   arrows: false,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   beforeChange: (current: any, next: any) => setActiveSlide(next),
+  //   afterChange: (current: any) => setAfterActiveSlide(current),
+  // };
   return (
     <div className="relative">
-      <Slider className="bg-yellow-100" {...settings}>
+      <Carousel>
+      <div>
+          <Image src={Image1} width={1920} height={1440} alt="slider-image" />
+        </div>
+        <div>
+          <Image src={Image2} width={1920} height={1440} alt="slider-image" />
+        </div>
+        <div>
+          <Image src={Image3} width={1920} height={1440} alt="slider-image" />
+        </div>
+      </Carousel>
+      {/* <Slider className="bg-yellow-100" {...settings}>
         <div>
           <Image src={Image1} width={1920} height={1440} alt="slider-image" />
         </div>
@@ -37,7 +49,7 @@ export default function TopSlick() {
       <div className="absolute top-0 bottom-0 right-0 left-0 z-10 gradient"></div>
       <TopSlickTexts activeSlide={activeSlide} num={0} text={"Balık"} />
       <TopSlickTexts activeSlide={activeSlide} num={1} text={"Cafe"} />
-      <TopSlickTexts activeSlide={activeSlide} num={2} text={"Restorant"} />
+      <TopSlickTexts activeSlide={activeSlide} num={2} text={"Restorant"} /> */}
     </div>
   );
 }
